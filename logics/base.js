57e4bd7c8c5015d88f944208413bbf8399073679
Matanel Mac <mmgordon.matanel@gmail.com>
2022-07-10 09:03:14 +0300

    implemented basic logic

--- logics/base.js
+++ logics/base.js
@@ -3,6 +3,9 @@ const {readTemplateFiles} = require("../utils/template");
 const TEMPLATE_PATH = 'templates/base';
 const baseLogic = async (fileManager, config) => {
     const files = await readTemplateFiles(TEMPLATE_PATH, config);
+    Object.entries(files).forEach(([name, content]) => {
+        fileManager.add(name, content);
+    })
     console.log(files);
 }
 
