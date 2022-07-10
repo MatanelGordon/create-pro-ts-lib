95123ede2671598a7373678773f61918a8a6c145
Matanel Mac <mmgordon.matanel@gmail.com>
2022-07-10 08:55:45 +0300

    created error with code

---
+++ utils/ErrorWithCode.js
@@ -0,0 +1,13 @@
+class ErrorWithCode extends Error{
+    #code;
+    constructor(msg, code) {
+        super(msg);
+        this.#code = code;
+    }
+
+    get code(){
+        return this.#code;
+    }
+}
+
+module.exports = ErrorWithCode;
