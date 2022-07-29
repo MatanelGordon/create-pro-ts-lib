0cfb9fc06195f9ea7b0ff6135ebc75aef0072dbd
Matanel Mac <mmgordon.matanel@gmail.com>
2022-07-29 14:02:13 +0300

    created wrapper for mv module

---
+++ utils/mvWrapper.js
@@ -0,0 +1,12 @@
+const _mv = require('mv');
+
+async function mv(source, dest){
+    return new Promise((res, rej) => {
+        _mv(source, dest, {mkdirp: true},function (err){
+            if(err) return rej(err);
+            res();
+        })
+    })
+}
+
+module.exports = mv;
