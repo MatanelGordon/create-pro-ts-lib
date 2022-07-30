479ac2b1efdabee81877db4b1df741291fa16bfc
Matanel Mac <mmgordon.matanel@gmail.com>
2022-07-30 15:02:10 +0300

    added src-dir modification to nodemon logic

--- logics/nodemon.js
+++ logics/nodemon.js
@@ -1,6 +1,15 @@
+const path = require('path');
 const { createTemplateFilesDownloader } = require('../utils/template');
-const TEMPLATE_PATH = 'templates/nodemon';
+const {DEFAULT_SOURCE_DIR} = require("./src-dir");
 
-const nodemonLogic = createTemplateFilesDownloader(TEMPLATE_PATH);
+const TEMPLATE_PATH = 'templates/nodemon';
+const downloadNodemon = createTemplateFilesDownloader(TEMPLATE_PATH);
+
+const nodemonLogic = async (filesManager, config, {flags}) => {
+    await downloadNodemon(filesManager, config);
+    const srcDir = flags['src-dir']?.value ?? DEFAULT_SOURCE_DIR;
+    const dev = `nodemon ${path.join(srcDir, 'index.ts')}`
+    filesManager.add('package.json', {scripts: {dev}});
+}
 
 module.exports = { nodemonLogic, TEMPLATE_PATH };
