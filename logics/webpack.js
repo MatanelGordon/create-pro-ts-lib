789797ac22b80efe4d78ff4b8c592e3a14f44e3d
Matanel Mac <mmgordon.matanel@gmail.com>
2023-04-29 13:53:44 +0300

    Added webpack logic and template

---
+++ logics/webpack.js
@@ -0,0 +1,13 @@
+const FilesManager = require("../utils/FilesManager");
+const { createTemplateFilesDownloader } = require('../utils/template');
+
+const TEMPLATE_PATH = 'templates/webpack';
+
+/**
+ * 
+ * @param {FilesManager} filesManager 
+ * @param {object} config
+ */
+const webpackLogic = createTemplateFilesDownloader(TEMPLATE_PATH)
+
+module.exports = webpackLogic;
